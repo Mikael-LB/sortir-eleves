e1c65bf1adb6808f898d6c2a30c0fd0afcb0e1a0
tkervran2021 <t.kervran@laposte.net>
2021-07-06 16:11:55 +0200

    Commit avec la créations des fixtures. Fonctionnel. Manque Lieu, Sortie, AssosPartiSort.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,128 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Campus;
+use App\Entity\Etat;
+use App\Entity\Participant;
+use App\Entity\Ville;
+use App\Repository\AssosPartiSortRepository;
+use App\Repository\CampusRepository;
+use App\Repository\EtatRepository;
+use App\Repository\LieuRepository;
+use App\Repository\ParticipantRepository;
+use App\Repository\SortieRepository;
+use App\Repository\VilleRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $NOM_CAMPUS = ['Rennes','Quimper','Niort','Nantes'];
+    protected $NOM_ETAT = ['En Création','Ouverte','Fermée','En Cours', 'Terminée', 'Annulée','Historisée'];
+    protected $campusRepository;
+    protected $villeRepository;
+    protected $lieuRepository;
+    protected $etatRepository;
+    protected $participantRepository;
+    protected $sortieRepository;
+    protected $assosPartiSortRepository;
+    protected $passwordEncoder;
+
+
+    public function __construct(CampusRepository $campusRepository,
+                                VilleRepository $villeRepository,
+                                LieuRepository $lieuRepository,
+                                EtatRepository $etatRepository,
+                                ParticipantRepository $participantRepository,
+                                SortieRepository $sortieRepository,
+                                AssosPartiSortRepository $assosPartiSortRepository,
+                                UserPasswordEncoderInterface $passwordEncoder
+    ){
+    $this->campusRepository = $campusRepository;
+    $this->villeRepository = $villeRepository;
+    $this->lieuRepository = $lieuRepository;
+    $this->etatRepository = $etatRepository;
+    $this->participantRepository = $participantRepository;
+    $this->sortieRepository= $sortieRepository;
+    $this->assosPartiSortRepository =$assosPartiSortRepository;
+    $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $generator = Factory::create('fr_FR');
+
+
+        //Création des campus
+//        $NOM_CAMPUS = ["Rennes", "Quimper", "Niort", "Nantes"];
+
+        foreach ($this->NOM_CAMPUS as $nomCampus) {
+            $campus = new Campus();
+            $campus->setNom(strval($nomCampus));
+
+            $manager->persist($campus);
+        }
+        $manager->flush();
+
+        //On récupère les campus
+        $campusList = $this->campusRepository->findAll();
+        $nbCampus = count($campusList);
+
+        //Création des participants
+        for ($i = 0; $i < $nbCampus; $i++) {
+            for ($j = 0; $j< 5; $j++){
+                $participant= new Participant();
+                $participant->setNom($generator->lastName)
+                    ->setEmail($generator->email)
+                    ->setPrenom($generator->firstName)
+                    ->setPassword($this->passwordEncoder->encodePassword($participant, 'password'))
+                    ->setTelephone($generator->phoneNumber)
+                    ->setPseudo($generator->userName)
+                    ->setEstAdministrateur(false)
+                    ->setCompteActif(true)
+                    ->setCampus($campusList[$i])
+                ;
+                $manager->persist($participant);
+            }
+        }
+        $manager->flush();
+
+        // On récupère tout nos participants
+        $participantList = $this->participantRepository->findAll();
+        $nbParticipantTotal = count($participantList);
+
+        // Création des villes
+
+        for ($i=0; $i<15 ; $i++){
+            $ville = new Ville();
+            $ville->setNom($generator->city)
+                ->setCodePostal($generator->postcode)
+            ;
+            $manager->persist($ville);
+        }
+        $manager->flush();
+        // On récupère les villes
+        $villeList = $this->villeRepository->findAll();
+        $nbVilles = count($villeList);
+
+
+        // Création des états
+
+
+        foreach ($this->NOM_ETAT as $etatNom) {
+            $etat = new Etat();
+            $etat->setLibelle(strval($etatNom));
+
+            $manager->persist($etat);
+        }
+        $manager->flush();
+
+        //On récupère les états
+        $etatList = $this->etatRepository->findAll();
+        $nbEtat =count($etatList);
+
 
         $manager->flush();
     }
