18805e5fad7eeeb07d1070f7248cea43f4adac03
tkervran2021 <t.kervran@laposte.net>
2021-07-08 15:52:41 +0200

    commit controller before pull

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\BO\Filtrer;
+use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Form\FiltrerType;
 use App\Form\SortieType;
@@ -10,6 +11,7 @@ use App\Repository\EtatRepository;
 use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Util\Type;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -52,8 +54,36 @@ class SortieController extends AbstractController
     }
 
     #[Route('/sorties/creer', name: 'sortie_creer')]
-    public function creerSortie(){
+    public function creerSortie(SortieRepository $sortieRepository, EntityManagerInterface $entityManager,Request $request): Response{
+        $sortie = new Sortie();
+        /**
+         * @var (type= Participant)
+         */
+        $organisateur = $this->getUser();
 
+        $sortie->setParticipant($organisateur);
+        $sortie->setCampus($organisateur->getCampus());
+
+        $sortieForm = $this->createForm(SortieType::class,$sortie);
+
+        $sortieForm->handleRequest($request);
+
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()){
+
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Sortie créée avec Succès');
+
+            // A modifier et rediriger vers la visualisation de sortie détail
+            return $this->redirectToRoute('sortie_liste_sorties');
+        }
+
+        return $this->render('sortie/creer-sortie.html.twig', [
+            'sortieForm' => $sortieForm->createView(),
+            'campus' => $sortie->getCampus()->getNom(),
+
+        ]);
 
     }
 
